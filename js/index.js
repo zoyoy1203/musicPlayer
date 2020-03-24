e9542afe456212fc9a11092558c7fd63a093d862
zoyoy1203 <52310049+zoyoy1203@users.noreply.github.com>
2020-03-25 00:22:07 +0800

    添加自动播放功能

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ $(function(){
     var musicImgsData = ['img/bg.jpg','img/bg1.jpg','img/bg2.jpg']    // 图片地址数组
     var musicNameData = ['出山','盗将行','归去来兮'];                   // 歌曲名数组
     var artistNameData = ['花粥/王胜娚','花粥/马雨阳','花粥']            // 创作歌手数组
-    var musicUrls=['mp23/music1.mp3','mp3/music2.mp3','mp3/music3.mp3'];// 歌曲mp3数组
+    var musicUrls=['mp3/music1.mp3','mp3/music2.mp3','mp3/music3.mp3'];// 歌曲mp3数组
     var currIndex = -1;              // 当前播放索引
     
     var buffInterval = null          // 初始化定时器 判断是否需要缓冲
@@ -152,6 +152,8 @@ $(function(){
             tProgress.text('00:00');       // 播放时间重置为 00:00
             musicImgs.removeClass('buffering').removeClass('active');  // 移除相关类名
             clearInterval(buffInterval);   // 清除定时器
+
+            selectTrack(1);  // 添加这一句，可以实现自动播放
 		}
     }
 
